01ebd57dae67fbfbe692895af62c870981e39e14
Nathaniel Patterson <odst0016@gmail.com>
2021-05-31 17:29:32 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const subscribersRouter = require("./routes/subscribers");
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+
+app.use("/subscribers", subscribersRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
